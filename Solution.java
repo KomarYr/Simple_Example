61777d3f7a9f77849ca8f492a3ed910f64574e58
KomarYr <93538763+KomarYr@users.noreply.github.com>
2022-10-22 22:10:45 +0100

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,23 @@
+package Simple_Example;
+
+public class Solution {
+    public static void main(String[] arg)   {
+        System.out.println(2 * 5);
+        short x = -32768;
+        short y = 6;
+        System.out.println(x + y);
+        float z = 10.86f;
+        float h = 16.32f;
+        System.out.println(z / h);
+        byte Num1 = -128;
+        byte Num2 = 127;
+        byte Num3 = (byte) (Num1 + Num2);
+        System.out.println(Num3);
+        long Num4 = 9223372036854775807L;
+        long Num5 = 3L;
+        System.out.println(Num4 / Num5);
+        double Num6 = 3.14159265359d;
+        double Num7 = 6.62607015d;
+        System.out.println(Num7 % Num6);
+    }
+}
